2a2a8c5188bb0b4ff5a3e33fd08c4847a6e8d4d0
Dmitri Mendis <2dmendis@gmail.com>
2018-05-08 19:12:44 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,5 @@
 def my_hash
   new_hash = {"name" => "Dmitri Mendis", "address" => "30 Duchess Drive"}
-  
 end
 
 
@@ -10,9 +9,6 @@ def shipping_manifest
     "porcelain vases" => 2 
     "oil paintings" => 3 
   }
-  # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
 end
 
 def retrieval
@@ -21,8 +17,6 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
   shipping_manifest["oil paintings"]
 
 end
@@ -33,15 +27,9 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-                 
-  # add 2 muskets to the shipping_manifest hash below
+
 shipping_manifest["muskets"] = 2 
 shipping_manifest["gun powder"] = 4
 shipping_manifest
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
 
 end
